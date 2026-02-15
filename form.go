601ee9a892afa1774eff879c17687ee45261acdf
Roman Khrebtov <khrebtov.dev@gmail.com>
2026-02-15 18:17:02 +0300

    Parse form

--- form.go
+++ form.go
@@ -1,5 +1,12 @@
 package xforms
 
+import (
+	"encoding/xml"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
 type Form struct {
 	Schema      FormSchema
 	Instance    FormInstance
@@ -9,29 +16,743 @@ type Form struct {
 }
 
 type FormSchema struct {
-	// TODO
+	TargetNamespace string
+
+	// All maps are keyed by local name (QName prefix is ignored).
+	SimpleTypes  map[string]*FormSchemaSimpleType
+	ComplexTypes map[string]*FormSchemaComplexType
+	Elements     map[string]*FormSchemaElementDecl
 }
 
 type FormSchemaSimpleType struct {
-	// TODO
+	Name         string
+	BaseTypeQName string // e.g. xsd:string, xsd:decimal, xsd:boolean
+
+	Pattern      *string
+	Enumeration  []string
+	MinLength    *int
+	MaxLength    *int
+	TotalDigits  *int
+	FractionDigits *int
+
+	// Numeric facets are kept as raw strings and interpreted according to base type.
+	MinInclusive *string
+	MaxInclusive *string
+	MinExclusive *string
+	MaxExclusive *string
 }
 
 type FormSchemaComplexType struct {
-	// TODO
+	Name string
+
+	// For initial scope, we support xsd:all.
+	All []FormSchemaElementDecl
 }
 
 type FormInstance struct {
-	// TODO
+	Root   xml.Name
+	Fields map[string]FormInstanceField // keyed by local name
 }
 
 type FormBind struct {
-	// TODO
+	ID      *string
+	Nodeset string
+
+	// TypeQName is either an xsd builtin type (xsd:string/decimal/boolean)
+	// or a schema type (e.g. a-3:PERSONAL_ACCOUNT_1_1). Prefix is preserved.
+	TypeQName *string
+
+	ExtType *string
+
+	Required bool
+	Readonly bool
+	Relevant bool
+
+	// Raw expressions as provided in XML (useful for debugging).
+	RawRequired string
+	RawReadonly string
+	RawRelevant string
 }
 
 type FormSubmission struct {
-	// TODO
+	ID     string
+	Action string
+	Method string
 }
 
 type FormBody struct {
-	// TODO
+	Elements []FormBodyElement
+}
+
+type FormSchemaElementDecl struct {
+	Name      string
+	TypeQName string // may be empty if ComplexType is provided inline
+	Nillable  *bool
+
+	ComplexType *FormSchemaComplexType
+}
+
+type FormInstanceField struct {
+	Name      string
+	Namespace string
+	Value     string
+	IsNil     bool
+}
+
+func (fi FormInstance) Clone() FormInstance {
+	out := FormInstance{
+		Root:   fi.Root,
+		Fields: map[string]FormInstanceField{},
+	}
+	for k, v := range fi.Fields {
+		out.Fields[k] = v
+	}
+	return out
+}
+
+// FormBodyElement is an internal parsed representation of xforms controls.
+// It is later converted into user-facing []FormElement by (*Form).Elements().
+type FormBodyElement interface {
+	formBodyElement()
+}
+
+type FormBodyGroup struct {
+	ID       *string
+	Label    string
+	Elements []FormBodyElement
+}
+
+func (*FormBodyGroup) formBodyElement() {}
+
+type FormBodyInput struct {
+	ID          *string
+	Ref         string
+	Incremental *bool
+
+	Label string
+	Alert string
+	Help  string
+	Hint  string
+}
+
+func (*FormBodyInput) formBodyElement() {}
+
+type FormBodySelect struct {
+	ID       *string
+	Ref      string
+	Multiple bool // true for xforms:select, false for xforms:select1
+
+	Label string
+	Alert string
+	Help  string
+	Hint  string
+
+	Items []FormBodySelectItem
+}
+
+type FormBodySelectItem struct {
+	Label string
+	Value string
+}
+
+func (*FormBodySelect) formBodyElement() {}
+
+type FormBodyOutput struct {
+	ID  *string
+	Ref *string
+
+	// For simplicity, we treat output as a message. If Ref is set, message
+	// can be taken from instance during conversion.
+	Label string
+}
+
+func (*FormBodyOutput) formBodyElement() {}
+
+func (s *FormSchema) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	*s = FormSchema{
+		SimpleTypes:  map[string]*FormSchemaSimpleType{},
+		ComplexTypes: map[string]*FormSchemaComplexType{},
+		Elements:     map[string]*FormSchemaElementDecl{},
+	}
+	for _, a := range start.Attr {
+		if a.Name.Local == "targetNamespace" {
+			s.TargetNamespace = a.Value
+			break
+		}
+	}
+
+	for {
+		tok, err := d.Token()
+		if err != nil {
+			return err
+		}
+		switch t := tok.(type) {
+		case xml.StartElement:
+			switch t.Name.Local {
+			case "simpleType":
+				var st xsdSimpleTypeXML
+				if err := d.DecodeElement(&st, &t); err != nil {
+					return err
+				}
+				conv, err := st.toForm()
+				if err != nil {
+					return err
+				}
+				if conv.Name != "" {
+					s.SimpleTypes[qnameLocal(conv.Name)] = conv
+				}
+			case "complexType":
+				var ct xsdComplexTypeXML
+				if err := d.DecodeElement(&ct, &t); err != nil {
+					return err
+				}
+				conv, err := ct.toForm()
+				if err != nil {
+					return err
+				}
+				if conv.Name != "" {
+					s.ComplexTypes[qnameLocal(conv.Name)] = conv
+				}
+			case "element":
+				var el xsdElementXML
+				if err := d.DecodeElement(&el, &t); err != nil {
+					return err
+				}
+				conv, err := el.toForm()
+				if err != nil {
+					return err
+				}
+				if conv.Name != "" {
+					s.Elements[qnameLocal(conv.Name)] = conv
+				}
+			default:
+				if err := d.Skip(); err != nil {
+					return err
+				}
+			}
+		case xml.EndElement:
+			if t.Name.Local == start.Name.Local {
+				return nil
+			}
+		}
+	}
+}
+
+func (fi *FormInstance) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	*fi = FormInstance{Fields: map[string]FormInstanceField{}}
+
+	for {
+		tok, err := d.Token()
+		if err != nil {
+			return err
+		}
+		switch t := tok.(type) {
+		case xml.StartElement:
+			// Root element of the instance (e.g. a-3:xmlData)
+			fi.Root = t.Name
+			if err := fi.decodeInstanceRoot(d, t); err != nil {
+				return err
+			}
+		case xml.EndElement:
+			if t.Name.Local == start.Name.Local {
+				return nil
+			}
+		}
+	}
+}
+
+func (fi *FormInstance) decodeInstanceRoot(d *xml.Decoder, root xml.StartElement) error {
+	for {
+		tok, err := d.Token()
+		if err != nil {
+			return err
+		}
+		switch t := tok.(type) {
+		case xml.StartElement:
+			// Direct child field under the instance root.
+			field := FormInstanceField{
+				Name:      t.Name.Local,
+				Namespace: t.Name.Space,
+			}
+			for _, a := range t.Attr {
+				if a.Name.Local == "nil" && (a.Name.Space == "http://www.w3.org/2001/XMLSchema-instance" || a.Name.Space == "") {
+					// Best-effort; we also accept missing namespace for practicality.
+					b, err := strconv.ParseBool(a.Value)
+					if err == nil {
+						field.IsNil = b
+					}
+				}
+			}
+			var cd struct {
+				Text string `xml:",chardata"`
+			}
+			if err := d.DecodeElement(&cd, &t); err != nil {
+				return err
+			}
+			field.Value = cd.Text
+			fi.Fields[t.Name.Local] = field
+		case xml.EndElement:
+			if t.Name.Local == root.Name.Local {
+				return nil
+			}
+		}
+	}
+}
+
+func (b *FormBody) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
+	*b = FormBody{}
+	for {
+		tok, err := d.Token()
+		if err != nil {
+			return err
+		}
+		switch t := tok.(type) {
+		case xml.StartElement:
+			el, err := decodeFormBodyElement(d, t)
+			if err != nil {
+				return err
+			}
+			if el != nil {
+				b.Elements = append(b.Elements, el)
+			}
+		case xml.EndElement:
+			if t.Name.Local == start.Name.Local {
+				return nil
+			}
+		}
+	}
+}
+
+func decodeFormBodyElement(d *xml.Decoder, start xml.StartElement) (FormBodyElement, error) {
+	switch start.Name.Local {
+	case "group":
+		g := &FormBodyGroup{}
+		for _, a := range start.Attr {
+			if a.Name.Local == "id" {
+				v := a.Value
+				g.ID = &v
+				break
+			}
+		}
+		for {
+			tok, err := d.Token()
+			if err != nil {
+				return nil, err
+			}
+			switch t := tok.(type) {
+			case xml.StartElement:
+				switch t.Name.Local {
+				case "label":
+					txt, err := decodeTextElement(d, t)
+					if err != nil {
+						return nil, err
+					}
+					g.Label = txt
+				default:
+					child, err := decodeFormBodyElement(d, t)
+					if err != nil {
+						return nil, err
+					}
+					if child != nil {
+						g.Elements = append(g.Elements, child)
+					}
+				}
+			case xml.EndElement:
+				if t.Name.Local == start.Name.Local {
+					return g, nil
+				}
+			}
+		}
+
+	case "input":
+		in := &FormBodyInput{}
+		for _, a := range start.Attr {
+			switch a.Name.Local {
+			case "id":
+				v := a.Value
+				in.ID = &v
+			case "ref":
+				in.Ref = a.Value
+			case "incremental":
+				// Optional, ignore parse errors (leave nil).
+				if bv, err := strconv.ParseBool(a.Value); err == nil {
+					in.Incremental = &bv
+				}
+			}
+		}
+		for {
+			tok, err := d.Token()
+			if err != nil {
+				return nil, err
+			}
+			switch t := tok.(type) {
+			case xml.StartElement:
+				switch t.Name.Local {
+				case "label":
+					txt, err := decodeTextElement(d, t)
+					if err != nil {
+						return nil, err
+					}
+					in.Label = txt
+				case "alert":
+					txt, err := decodeTextElement(d, t)
+					if err != nil {
+						return nil, err
+					}
+					in.Alert = txt
+				case "help":
+					txt, err := decodeTextElement(d, t)
+					if err != nil {
+						return nil, err
+					}
+					in.Help = txt
+				case "hint":
+					txt, err := decodeTextElement(d, t)
+					if err != nil {
+						return nil, err
+					}
+					in.Hint = txt
+				default:
+					if err := d.Skip(); err != nil {
+						return nil, err
+					}
+				}
+			case xml.EndElement:
+				if t.Name.Local == start.Name.Local {
+					if strings.TrimSpace(in.Ref) == "" {
+						// Ref-less inputs are not supported in the initial scope.
+						return nil, nil
+					}
+					return in, nil
+				}
+			}
+		}
+
+	case "select1", "select":
+		sel := &FormBodySelect{Multiple: start.Name.Local == "select"}
+		for _, a := range start.Attr {
+			switch a.Name.Local {
+			case "id":
+				v := a.Value
+				sel.ID = &v
+			case "ref":
+				sel.Ref = a.Value
+			}
+		}
+		for {
+			tok, err := d.Token()
+			if err != nil {
+				return nil, err
+			}
+			switch t := tok.(type) {
+			case xml.StartElement:
+				switch t.Name.Local {
+				case "label":
+					txt, err := decodeTextElement(d, t)
+					if err != nil {
+						return nil, err
+					}
+					sel.Label = txt
+				case "alert":
+					txt, err := decodeTextElement(d, t)
+					if err != nil {
+						return nil, err
+					}
+					sel.Alert = txt
+				case "help":
+					txt, err := decodeTextElement(d, t)
+					if err != nil {
+						return nil, err
+					}
+					sel.Help = txt
+				case "hint":
+					txt, err := decodeTextElement(d, t)
+					if err != nil {
+						return nil, err
+					}
+					sel.Hint = txt
+				case "item":
+					item, err := decodeSelectItem(d, t)
+					if err != nil {
+						return nil, err
+					}
+					sel.Items = append(sel.Items, item)
+				default:
+					if err := d.Skip(); err != nil {
+						return nil, err
+					}
+				}
+			case xml.EndElement:
+				if t.Name.Local == start.Name.Local {
+					if strings.TrimSpace(sel.Ref) == "" {
+						return nil, nil
+					}
+					return sel, nil
+				}
+			}
+		}
+
+	case "output":
+		out := &FormBodyOutput{}
+		for _, a := range start.Attr {
+			switch a.Name.Local {
+			case "id":
+				v := a.Value
+				out.ID = &v
+			case "ref":
+				v := a.Value
+				out.Ref = &v
+			}
+		}
+		for {
+			tok, err := d.Token()
+			if err != nil {
+				return nil, err
+			}
+			switch t := tok.(type) {
+			case xml.StartElement:
+				switch t.Name.Local {
+				case "label":
+					txt, err := decodeTextElement(d, t)
+					if err != nil {
+						return nil, err
+					}
+					out.Label = txt
+				default:
+					if err := d.Skip(); err != nil {
+						return nil, err
+					}
+				}
+			case xml.EndElement:
+				if t.Name.Local == start.Name.Local {
+					return out, nil
+				}
+			}
+		}
+
+	default:
+		// Skip unsupported controls (e.g. submit) for now.
+		if err := d.Skip(); err != nil {
+			return nil, err
+		}
+		return nil, nil
+	}
+}
+
+func decodeTextElement(d *xml.Decoder, start xml.StartElement) (string, error) {
+	var cd struct {
+		Text string `xml:",chardata"`
+	}
+	if err := d.DecodeElement(&cd, &start); err != nil {
+		return "", err
+	}
+	return strings.TrimSpace(cd.Text), nil
+}
+
+func decodeSelectItem(d *xml.Decoder, start xml.StartElement) (FormBodySelectItem, error) {
+	var item FormBodySelectItem
+	for {
+		tok, err := d.Token()
+		if err != nil {
+			return item, err
+		}
+		switch t := tok.(type) {
+		case xml.StartElement:
+			switch t.Name.Local {
+			case "label":
+				txt, err := decodeTextElement(d, t)
+				if err != nil {
+					return item, err
+				}
+				item.Label = txt
+			case "value":
+				txt, err := decodeTextElement(d, t)
+				if err != nil {
+					return item, err
+				}
+				item.Value = txt
+			default:
+				if err := d.Skip(); err != nil {
+					return item, err
+				}
+			}
+		case xml.EndElement:
+			if t.Name.Local == start.Name.Local {
+				return item, nil
+			}
+		}
+	}
+}
+
+func qnameLocal(qname string) string {
+	if i := strings.IndexByte(qname, ':'); i >= 0 {
+		return qname[i+1:]
+	}
+	return qname
+}
+
+func parseOptionalBoolAttr(v string) (*bool, error) {
+	v = strings.TrimSpace(v)
+	if v == "" {
+		return nil, nil
+	}
+	b, err := strconv.ParseBool(v)
+	if err != nil {
+		return nil, err
+	}
+	return &b, nil
+}
+
+func parseXFormsBoolExpr(expr string, defaultValue bool) (bool, error) {
+	expr = strings.TrimSpace(expr)
+	if expr == "" {
+		return defaultValue, nil
+	}
+	switch expr {
+	case "true", "true()":
+		return true, nil
+	case "false", "false()":
+		return false, nil
+	default:
+		// Keep the surface area small for v1.
+		return false, fmt.Errorf("unsupported boolean expression %q (supported: true/false/true()/false())", expr)
+	}
+}
+
+// --- Internal XML helpers for XSD parsing ---
+
+type xsdFacetXML struct {
+	Value string `xml:"value,attr"`
+}
+
+type xsdRestrictionXML struct {
+	Base string `xml:"base,attr"`
+
+	Pattern        *xsdFacetXML  `xml:"pattern"`
+	Enumeration    []xsdFacetXML `xml:"enumeration"`
+	MinLength      *xsdFacetXML  `xml:"minLength"`
+	MaxLength      *xsdFacetXML  `xml:"maxLength"`
+	TotalDigits    *xsdFacetXML  `xml:"totalDigits"`
+	FractionDigits *xsdFacetXML  `xml:"fractionDigits"`
+
+	MinInclusive *xsdFacetXML `xml:"minInclusive"`
+	MaxInclusive *xsdFacetXML `xml:"maxInclusive"`
+	MinExclusive *xsdFacetXML `xml:"minExclusive"`
+	MaxExclusive *xsdFacetXML `xml:"maxExclusive"`
+}
+
+type xsdSimpleTypeXML struct {
+	Name        string            `xml:"name,attr"`
+	Restriction xsdRestrictionXML `xml:"restriction"`
+}
+
+func (st xsdSimpleTypeXML) toForm() (*FormSchemaSimpleType, error) {
+	out := &FormSchemaSimpleType{
+		Name:          st.Name,
+		BaseTypeQName: st.Restriction.Base,
+	}
+	if st.Restriction.Pattern != nil {
+		v := st.Restriction.Pattern.Value
+		out.Pattern = &v
+	}
+	for _, e := range st.Restriction.Enumeration {
+		out.Enumeration = append(out.Enumeration, e.Value)
+	}
+	if st.Restriction.MinLength != nil {
+		i, err := strconv.Atoi(strings.TrimSpace(st.Restriction.MinLength.Value))
+		if err != nil {
+			return nil, err
+		}
+		out.MinLength = &i
+	}
+	if st.Restriction.MaxLength != nil {
+		i, err := strconv.Atoi(strings.TrimSpace(st.Restriction.MaxLength.Value))
+		if err != nil {
+			return nil, err
+		}
+		out.MaxLength = &i
+	}
+	if st.Restriction.TotalDigits != nil {
+		i, err := strconv.Atoi(strings.TrimSpace(st.Restriction.TotalDigits.Value))
+		if err != nil {
+			return nil, err
+		}
+		out.TotalDigits = &i
+	}
+	if st.Restriction.FractionDigits != nil {
+		i, err := strconv.Atoi(strings.TrimSpace(st.Restriction.FractionDigits.Value))
+		if err != nil {
+			return nil, err
+		}
+		out.FractionDigits = &i
+	}
+	if st.Restriction.MinInclusive != nil {
+		v := st.Restriction.MinInclusive.Value
+		out.MinInclusive = &v
+	}
+	if st.Restriction.MaxInclusive != nil {
+		v := st.Restriction.MaxInclusive.Value
+		out.MaxInclusive = &v
+	}
+	if st.Restriction.MinExclusive != nil {
+		v := st.Restriction.MinExclusive.Value
+		out.MinExclusive = &v
+	}
+	if st.Restriction.MaxExclusive != nil {
+		v := st.Restriction.MaxExclusive.Value
+		out.MaxExclusive = &v
+	}
+	return out, nil
+}
+
+type xsdAllXML struct {
+	Elements []xsdElementXML `xml:"element"`
+}
+
+type xsdComplexTypeXML struct {
+	Name string `xml:"name,attr"`
+
+	All *xsdAllXML `xml:"all"`
+}
+
+func (ct xsdComplexTypeXML) toForm() (*FormSchemaComplexType, error) {
+	out := &FormSchemaComplexType{Name: ct.Name}
+	if ct.All != nil {
+		for _, el := range ct.All.Elements {
+			conv, err := el.toForm()
+			if err != nil {
+				return nil, err
+			}
+			out.All = append(out.All, *conv)
+		}
+	}
+	return out, nil
+}
+
+type xsdElementXML struct {
+	Name     string `xml:"name,attr"`
+	Type     string `xml:"type,attr"`
+	Nillable string `xml:"nillable,attr"`
+
+	ComplexType *xsdComplexTypeXML `xml:"complexType"`
+}
+
+func (el xsdElementXML) toForm() (*FormSchemaElementDecl, error) {
+	out := &FormSchemaElementDecl{
+		Name:      el.Name,
+		TypeQName: el.Type,
+	}
+	if b, err := parseOptionalBoolAttr(el.Nillable); err != nil {
+		return nil, err
+	} else {
+		out.Nillable = b
+	}
+	if el.ComplexType != nil {
+		ct, err := el.ComplexType.toForm()
+		if err != nil {
+			return nil, err
+		}
+		out.ComplexType = ct
+	}
+	return out, nil
 }
---
+++ parse_test.go
@@ -0,0 +1,206 @@
+package xforms
+
+import (
+	"encoding/xml"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+const sampleXFormsXML = `<?xml version="1.0" encoding="UTF-8"?>
+<xhtml:html xmlns:a-3="http://www.a-3.ru/xforms/schema" xmlns:xforms="http://www.w3.org/2002/xforms" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchemainstance" xsi:schemaLocation="http://www.w3.org/2002/xforms http://www.w3.org/MarkUp/Forms/2002/XForms-Schema.xsd">
+  <xhtml:head>
+    <xforms:model>
+      <xsd:schema targetNamespace="http://www.a-3.ru/xforms/schema">
+        <xsd:simpleType name="PERSONAL_ACCOUNT_1_1">
+          <xsd:restriction base="xsd:string">
+            <xsd:pattern value="^\d{10}$"/>
+          </xsd:restriction>
+        </xsd:simpleType>
+        <xsd:simpleType name="TERMINALID_2_1">
+          <xsd:restriction base="xsd:string"/>
+        </xsd:simpleType>
+        <xsd:simpleType name="_CHECK_TEXT__3_1">
+          <xsd:restriction base="xsd:string"/>
+        </xsd:simpleType>
+        <xsd:complexType name="SumCheck">
+          <xsd:all>
+            <xsd:element name="sum" type="xsd:decimal"/>
+            <xsd:element name="check" type="xsd:boolean"/>
+          </xsd:all>
+        </xsd:complexType>
+        <xsd:element name="xmlData">
+          <xsd:complexType>
+            <xsd:all>
+              <xsd:element name="transactionId" nillable="false" type="xsd:string"/>
+              <xsd:element name="a3_PERSONAL_ACCOUNT_1_1" nillable="false" type="a-3:PERSONAL_ACCOUNT_1_1"/>
+              <xsd:element name="a3_TERMINALID_2_1" nillable="false" type="a-3:TERMINALID_2_1"/>
+              <xsd:element name="a3__CHECK_TEXT__3_1" nillable="true" type="a-3:_CHECK_TEXT__2_1"/>
+            </xsd:all>
+          </xsd:complexType>
+        </xsd:element>
+      </xsd:schema>
+      <xforms:instance>
+        <a-3:xmlData>
+          <transactionId>456803</transactionId>
+          <a3_PERSONAL_ACCOUNT_1_1>012345678</a3_PERSONAL_ACCOUNT_1_1>
+          <a3_TERMINALID_2_1>testTerminal01</a3_TERMINALID_2_1>
+          <a3__CHECK_TEXT__3_1/>
+        </a-3:xmlData>
+      </xforms:instance>
+      <xforms:bind exttype="PERSONAL_ACCOUNT" nodeset="a3_PERSONAL_ACCOUNT_1_1" readonly="false" relevant="true()" required="true" type="a-3:PERSONAL_ACCOUNT_1_1"/>
+      <xforms:bind exttype="PARAMETER" nodeset="a3_TERMINALID_2_1" readonly="false" relevant="false()" required="true" type="a-3:TERMINALID_2_1"/>
+      <xforms:bind exttype="CLIENT" nodeset="a3__CHECK_TEXT__3_1" readonly="true" relevant="false()" required="false" type="a-3:_CHECK_TEXT__3_1"/>
+      <xforms:submission action="http://localhost/" id="a-3.submission.back" method="get"/>
+      <xforms:submission action="http://localhost/" id="a-3.submission.next" method="get"/>
+      <xforms:submission action="http://localhost/" id="a-3.submission.pay" method="get"/>
+      <xforms:submission action="http://localhost/" id="a-3.submission.link" method="get"/>
+    </xforms:model>
+  </xhtml:head>
+  <xhtml:body>
+    <xforms:group id="10">
+      <xforms:label/>
+      <xforms:input id="PERSONAL_ACCOUNT_1_1" incremental="true" ref="a3_PERSONAL_ACCOUNT_1_1">
+        <xforms:label>Personal account number:</xforms:label>
+        <xforms:alert>Incorrect personal account number format!</xforms:alert>
+        <xforms:help>Example of completion: 1234567890</xforms:help>
+      </xforms:input>
+    </xforms:group>
+    <xforms:submit id="a-3.back" submission="a-3.submission.back">
+      <xforms:label>Back</xforms:label>
+    </xforms:submit>
+    <xforms:submit id="a-3.next" submission="a-3.submission.next">
+      <xforms:label>Next</xforms:label>
+    </xforms:submit>
+  </xhtml:body>
+</xhtml:html>
+`
+
+func TestParse_SampleXForms(t *testing.T) {
+	got, err := Parse(strings.NewReader(sampleXFormsXML))
+	if err != nil {
+		t.Fatalf("Parse() error: %v", err)
+	}
+
+	sp := func(s string) *string { return &s }
+	bp := func(b bool) *bool { return &b }
+
+	want := &Form{
+		Schema: FormSchema{
+			TargetNamespace: "http://www.a-3.ru/xforms/schema",
+			SimpleTypes: map[string]*FormSchemaSimpleType{
+				"PERSONAL_ACCOUNT_1_1": {
+					Name:          "PERSONAL_ACCOUNT_1_1",
+					BaseTypeQName: "xsd:string",
+					Pattern:       sp("^\\d{10}$"),
+				},
+				"TERMINALID_2_1": {
+					Name:          "TERMINALID_2_1",
+					BaseTypeQName: "xsd:string",
+				},
+				"_CHECK_TEXT__3_1": {
+					Name:          "_CHECK_TEXT__3_1",
+					BaseTypeQName: "xsd:string",
+				},
+			},
+			ComplexTypes: map[string]*FormSchemaComplexType{
+				"SumCheck": {
+					Name: "SumCheck",
+					All: []FormSchemaElementDecl{
+						{Name: "sum", TypeQName: "xsd:decimal"},
+						{Name: "check", TypeQName: "xsd:boolean"},
+					},
+				},
+			},
+			Elements: map[string]*FormSchemaElementDecl{
+				"xmlData": {
+					Name:      "xmlData",
+					TypeQName: "",
+					ComplexType: &FormSchemaComplexType{
+						Name: "",
+						All: []FormSchemaElementDecl{
+							{Name: "transactionId", TypeQName: "xsd:string", Nillable: bp(false)},
+							{Name: "a3_PERSONAL_ACCOUNT_1_1", TypeQName: "a-3:PERSONAL_ACCOUNT_1_1", Nillable: bp(false)},
+							{Name: "a3_TERMINALID_2_1", TypeQName: "a-3:TERMINALID_2_1", Nillable: bp(false)},
+							{Name: "a3__CHECK_TEXT__3_1", TypeQName: "a-3:_CHECK_TEXT__2_1", Nillable: bp(true)},
+						},
+					},
+				},
+			},
+		},
+		Instance: FormInstance{
+			Root: xml.Name{Space: "http://www.a-3.ru/xforms/schema", Local: "xmlData"},
+			Fields: map[string]FormInstanceField{
+				"transactionId":           {Name: "transactionId", Namespace: "", Value: "456803", IsNil: false},
+				"a3_PERSONAL_ACCOUNT_1_1": {Name: "a3_PERSONAL_ACCOUNT_1_1", Namespace: "", Value: "012345678", IsNil: false},
+				"a3_TERMINALID_2_1":       {Name: "a3_TERMINALID_2_1", Namespace: "", Value: "testTerminal01", IsNil: false},
+				"a3__CHECK_TEXT__3_1":     {Name: "a3__CHECK_TEXT__3_1", Namespace: "", Value: "", IsNil: false},
+			},
+		},
+		Binds: []*FormBind{
+			{
+				Nodeset:     "a3_PERSONAL_ACCOUNT_1_1",
+				TypeQName:   sp("a-3:PERSONAL_ACCOUNT_1_1"),
+				ExtType:     sp("PERSONAL_ACCOUNT"),
+				Required:    true,
+				Readonly:    false,
+				Relevant:    true,
+				RawRequired: "true",
+				RawReadonly: "false",
+				RawRelevant: "true()",
+			},
+			{
+				Nodeset:     "a3_TERMINALID_2_1",
+				TypeQName:   sp("a-3:TERMINALID_2_1"),
+				ExtType:     sp("PARAMETER"),
+				Required:    true,
+				Readonly:    false,
+				Relevant:    false,
+				RawRequired: "true",
+				RawReadonly: "false",
+				RawRelevant: "false()",
+			},
+			{
+				Nodeset:     "a3__CHECK_TEXT__3_1",
+				TypeQName:   sp("a-3:_CHECK_TEXT__3_1"),
+				ExtType:     sp("CLIENT"),
+				Required:    false,
+				Readonly:    true,
+				Relevant:    false,
+				RawRequired: "false",
+				RawReadonly: "true",
+				RawRelevant: "false()",
+			},
+		},
+		Submissions: []*FormSubmission{
+			{ID: "a-3.submission.back", Action: "http://localhost/", Method: "get"},
+			{ID: "a-3.submission.next", Action: "http://localhost/", Method: "get"},
+			{ID: "a-3.submission.pay", Action: "http://localhost/", Method: "get"},
+			{ID: "a-3.submission.link", Action: "http://localhost/", Method: "get"},
+		},
+		Body: FormBody{
+			Elements: []FormBodyElement{
+				&FormBodyGroup{
+					ID:    sp("10"),
+					Label: "",
+					Elements: []FormBodyElement{
+						&FormBodyInput{
+							ID:          sp("PERSONAL_ACCOUNT_1_1"),
+							Ref:         "a3_PERSONAL_ACCOUNT_1_1",
+							Incremental: bp(true),
+							Label:       "Personal account number:",
+							Alert:       "Incorrect personal account number format!",
+							Help:        "Example of completion: 1234567890",
+							Hint:        "",
+						},
+					},
+				},
+			},
+		},
+	}
+
+	if diff := cmp.Diff(want, got); diff != "" {
+		t.Fatalf("Parse() mismatch (-want +got):\n%s", diff)
+	}
+}
